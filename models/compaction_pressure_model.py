40e3ede723ad1fae41e7095cf6c436ddd4986fb6
Ethan Pierce <ethan.pierce@colorado.edu>
2024-09-09 14:07:02 -0600

    Add models dir

---
+++ models/compaction_pressure_model.py
@@ -0,0 +1,10 @@
+"""Model vertical temperature and enthalpy distribution.
+
+Uses the compaction-pressure model for englacial drainage introduced by 
+Hewitt and Schoof (2017). 
+"""
+
+class CompactionPressureModel:
+
+    def __init__(self):
+        pass
