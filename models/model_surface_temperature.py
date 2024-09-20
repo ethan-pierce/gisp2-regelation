a77f8a25a555cae55f355d26a8f5984bce350258
Ethan Pierce <ethan.pierce@colorado.edu>
2024-09-20 16:24:52 -0600

    Implement basal temp calculation

--- models/model_surface_temperature.py
+++ models/model_surface_temperature.py
@@ -12,7 +12,6 @@ alley_temp = alley_temp.dropna(axis = 1)
 alley_acc = pd.read_csv('data/alley2000-gisp.txt', skiprows = 1716, nrows = 3414-1717+1, header = 0, sep = '\s+')
 
 markle_all = loadmat('data/markle.mat')
-print(markle_all.keys())
 markle_grip = pd.DataFrame(markle_all['GRIP_T_surf'], columns = ['Temperature'])
 markle_grip['Age'] = markle_all['GRIP_age']
 markle_grip = markle_grip[markle_grip['Age'] <= 120]
@@ -54,13 +53,59 @@ Ts0 = 273 - 30
 
 T0 = Ts0 + (np.sqrt(np.pi) / 2) * c1 * G * (erf((np.max(zs) - zs) / c1) - erf(H / c1))
 
-
-
-
+def calc_steady_T(Ts, b):
+    b /= 31556926
+    c = np.sqrt(2 * kappa * H / b)
+    T = Ts + (np.sqrt(np.pi) / 2) * c * G * (erf((np.max(zs) - zs) / c) - erf(H / c))
+    return T
 
 fig, ax = plt.subplots(figsize = (12, 8))
-plt.plot(T0, zs)
-plt.xlabel('Temperature (°K)')
-plt.ylabel('Depth (m)')
+for i in range(kindler.shape[0]):
+    Ts = 273 + kindler['Temperature'].iloc[i]
+    b = kindler['Accumulation'].iloc[i]
+    T = calc_steady_T(Ts, b)
+
+    if i % 100 == 0:
+        plt.plot(T, zs, alpha = 0.1, color = 'dodgerblue')
 ax.invert_yaxis()
+plt.xlabel('Temperature (K)')
+plt.ylabel('Depth (m)')
+plt.show()
+quit()
+
+
+
+def calc_dT(T, Ts, b):
+    T[-1] = Ts
+    grad_T = np.gradient(T, dz, edge_order = 2)
+    grad_T[0] = G
+    w = -b * zs / H
+    diffusion = kappa * np.gradient(grad_T, dz, edge_order = 2)
+    advection = -w * grad_T
+    return diffusion + advection
+
+T = T0.copy()
+Tb = [T0[0]]
+fig, ax = plt.subplots(figsize = (12, 8))
+for i in range(1000):
+    Ts = 273 + kindler['Temperature'].iloc[i]
+    b = kindler['Accumulation'].iloc[i] / 31556926
+    
+    for j in range(2000):
+        dT = calc_dT(T, Ts, b)
+        T = T + dT
+    
+    dT = calc_dT(T, Ts, b)
+    T = T + dT * 31554926 # s/a - 2000 from spinup
+
+    Tb.append(T[0])
+
+    if i % 100 == 0:
+        plt.plot(T, zs, alpha = 0.1, color = 'dodgerblue')
+        print('%.2f %%' % (i / kindler.shape[0] * 100))
+
+ax.invert_yaxis()
+plt.xlabel('Temperature (K)')
+plt.ylabel('Depth (m)')
+plt.savefig('temperature_profile.png', dpi = 300)
 plt.show()
